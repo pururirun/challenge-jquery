c268819a72eb0db1c141d791c3e7496cc709c1d9
pururirun <j.mardonescava@gmail.com>
2018-01-08 16:02:48 -0300

    Etapa 3-Función ​ renderHighlightedRecipes

--- js/script-index.js
+++ js/script-index.js
@@ -17,7 +17,12 @@ $(function () {
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
+	for (var i = 0; i < recipesArray.length; i++) {
+		if (recipesArray[i].highlighted == true) {
+			renderRecipe(recipesArray[i])
+		}
+	}
+	//console.log('Recipes: ', recipesArray);
 }
 
 /*
